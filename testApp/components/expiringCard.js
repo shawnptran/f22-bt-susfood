c9f8c85b9ebe55822078780850a44f699d7ab7c9
shawnptran <shawntranusa@gmail.com>
2022-11-09 21:26:11 -0600

    updated home

--- testApp/components/expiringCard.js
+++ testApp/components/expiringCard.js
@@ -1,4 +1,4 @@
-import { View, StyleSheet, SafeAreaView, FlatList, Text } from 'react-native';
+import { View, StyleSheet, SafeAreaView, FlatList, Text, Image } from 'react-native';
  
 const TestCard = () => {
  
@@ -6,30 +6,37 @@ const TestCard = () => {
     {
       id: 1,
       name: 'Beef',
+      src:require('../icons/apple.png')
     },
     {
       id: 2,
       name: 'Lettuce',
+      src:require('../icons/red bell peppers.png')
     },
     {
       id: 3,
       name: 'Chicken',
+      src:require('../icons/zucchini.png')
     },
     {
       id: 4,
       name: 'Tomato',
+      src:require('../icons/random.png')
     },
     {
       id: 5,
       name: 'Avocado',
+      src:require('../icons/random.png')
     },
     {
       id: 6,
       name: 'Lemon',
+      src:require('../icons/random.png')
     },
     {
       id: 7,
       name: 'Lime',
+      src:require('../icons/random.png')
     }
  
   ];
@@ -56,12 +63,16 @@ const TestCard = () => {
     <SafeAreaView style={styleSheet.MainContainer}>
  
       <Text style={styleSheet.titleText}>
-        Horizontal FlatList in React Native
+        Expiring Soon
       </Text>
  
       <FlatList
         data={ANIMAL_NAMES}
-        renderItem={({ item }) => <ItemRender name={item.name} />}
+        renderItem={({ item }) => 
+        <View style={styleSheet.item}>
+          <Image source={item.src}style={styleSheet.image}></Image>
+        </View>
+      }
         keyExtractor={item => item.id}
         ItemSeparatorComponent={Separator}
         horizontal={true}
@@ -77,6 +88,11 @@ const styleSheet = StyleSheet.create({
   MainContainer: {
     flex: 1,
     backgroundColor: 'white'
+  },
+
+  image: {
+    height: 30,
+    width: 30,
   },
  
   titleText: {
