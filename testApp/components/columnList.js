758af9f098e4ca55a46de83a7c74a0e89b86d16a
your_Lynixaa <anayang02@utexas.edu>
2022-11-12 10:53:23 -0600

    working on search function in search page

--- testApp/components/columnList.js
+++ testApp/components/columnList.js
@@ -45,12 +45,80 @@ const TwoColumn = () => {
       src:require('../icons/random.png')
     },
   ];
+
+  const [search, setSearch] = useState('');
+  const [filteredDataSource, setFilteredDataSource] = useState([]);
+  const [masterDataSource, setMasterDataSource] = useState([]);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then((response) => response.json())
+      .then((responseJson) => {
+        setFilteredDataSource(responseJson);
+        setMasterDataSource(responseJson);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
+  const searchFilterFunction = (text) => {
+    // Check if searched text is not blank
+    if (text) {
+      // Inserted text is not blank
+      // Filter the masterDataSource and update FilteredDataSource
+      const newData = masterDataSource.filter(function (item) {
+        // Applying filter for the inserted text in search bar
+        const itemData = item.title
+          ? item.title.toUpperCase()
+          : ''.toUpperCase();
+        const textData = text.toUpperCase();
+        return itemData.indexOf(textData) > -1;
+      });
+      setFilteredDataSource(newData);
+      setSearch(text);
+    } else {
+      // Inserted text is blank
+      // Update FilteredDataSource with masterDataSource
+      setFilteredDataSource(masterDataSource);
+      setSearch(text);
+    }
+  };
+
+  const ItemView = ({ item }) => {
+    return (
+      // Flat List Item
+      <Text style={styles.itemStyle} onPress={() => getItem(item)}>
+        {item.id}
+        {'.'}
+        {item.title.toUpperCase()}
+      </Text>
+    );
+  };
+
+  const ItemSeparatorView = () => {
+    return (
+      // Flat List Item Separator
+      <View
+        style={{
+          height: 0.5,
+          width: '100%',
+          backgroundColor: '#C8C8C8',
+        }}
+      />
+    );
+  };
+
+  const getItem = (item) => {
+    // Function for click on an item
+    alert('Id : ' + item.id + ' Title : ' + item.title);
+  };
  
-  const getItem = (name) => {
+  // const getItem = (name) => {
 
-    Alert.alert(name);
+  //   Alert.alert(name);
 
-  }
+  // }
 
   const ImageType = ({ image }) => (
     <View style={styleSheet.item}>
@@ -83,7 +151,13 @@ const TwoColumn = () => {
       <Text style={styleSheet.titleText}>
         testing
       </Text>
- 
+      <TextInput
+          style={styles.textInputStyle}
+          onChangeText={(text) => searchFilterFunction(text)}
+          value={search}
+          underlineColorAndroid="transparent"
+          placeholder="Search Here"
+        />
       <FlatList
         style={{margin:20}}
         columnWrapperStyle={styleSheet.row}
@@ -159,7 +233,16 @@ const styleSheet = StyleSheet.create({
     color: 'black',
     textAlign: 'center',
     marginTop: 11,
-  }
+  },
+
+  textInputStyle: {
+    height: 40,
+    borderWidth: 1,
+    paddingLeft: 20,
+    margin: 5,
+    borderColor: '#009688',
+    backgroundColor: '#FFFFFF',
+  },
  
 });
 
